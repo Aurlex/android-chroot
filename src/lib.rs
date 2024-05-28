aaca6aaf4e0398da08e685ea516244574cdd50f2
Aurlex <aurlex1@gmail.com>
2024-05-28 20:25:22 +0100

    temp commit for testing on hardware

--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,40 @@
-use std::fs::{read_to_string, ReadDir};
-use std::io::{Error, ErrorKind, Result};
+use anyhow::Result;
 use std::path::{Path, PathBuf};
-use sys_mount::{Mount, MountFlags, SupportedFilesystems};
+use sys_mount::{Mount, MountFlags};
 
-mod config;
-pub use config::*;
-
-#[cfg(test)]
-mod tests;
+// mod config;
+// pub use config::*;
 
 #[inline(always)]
 pub fn validate_file(path: impl AsRef<Path>, directory: bool, exists: bool) -> Result<PathBuf> {
 	let path = path.as_ref().canonicalize()?;
 	if path.try_exists()? != exists {
-		return Err(Error::new(
-			ErrorKind::NotFound,
-			format!(
-				"File \"{path:?}\" {}found.",
-				if directory { "not " } else { "" }
-			),
-		));
+		if exists {
+			anyhow::bail!("File {path:?} does not exist when it should.")
+		} else {
+			anyhow::bail!("File {path:?} already exists.")
+		};
 	}
 	if path.is_dir() != directory {
-		return Err(Error::new(
-			ErrorKind::InvalidInput,
-			format!(
-				"File {path:?} is {}a directory.",
-				if directory { "not " } else { "" }
-			),
-		));
+		if directory {
+			anyhow::bail!("File {path:?} should not be a directory.")
+		} else {
+			anyhow::bail!("File {path:?} should be a directory.")
+		};
 	}
 	Ok(path)
-}
-#[inline(always)]
-pub fn ls(path: impl AsRef<Path>) -> Result<ReadDir> {
-	validate_file(path, true, true)?.read_dir()
-}
-#[inline(always)]
-pub fn cat(path: impl AsRef<Path>) -> Result<String> {
-	read_to_string(validate_file(path, false, true)?)
-}
-#[inline(always)]
-pub fn mount_loopback(
-	from: impl AsRef<Path>,
-	to: impl AsRef<Path>,
-	fstype: impl AsRef<str>,
-) -> Result<Mount> {
-	let from = validate_file(from, false, true)?;
-	let to = validate_file(to, true, true)?;
-	if !SupportedFilesystems::new()?.is_supported(fstype.as_ref()) {
-		return Err(Error::new(
-			ErrorKind::Unsupported,
-			format!("Filesystem \"{}\" is unsupported", fstype.as_ref()),
-		));
-	}
-	Mount::builder()
-		.fstype(fstype.as_ref())
-		.explicit_loopback()
-		.mount(from, to)
 }
 #[inline(always)]
 pub fn mount_bind(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<Mount> {
 	let from = validate_file(from, false, true)?;
 	let to = validate_file(to, true, true)?;
-	Mount::builder().flags(MountFlags::BIND).mount(from, to)
+	Ok(Mount::builder().flags(MountFlags::BIND).mount(from, to)?)
 }
 #[inline(always)]
 pub fn mount_fs(
-	from: impl AsRef<Path>,
-	to: impl AsRef<Path>,
-	fstype: impl AsRef<str>,
+	from: impl AsRef<Path>, to: impl AsRef<Path>, fs: impl AsRef<str>,
 ) -> Result<Mount> {
 	let from = validate_file(from, false, true)?;
 	let to = validate_file(to, true, true)?;
-	if !SupportedFilesystems::new()?.is_supported(fstype.as_ref()) {
-		return Err(Error::new(
-			ErrorKind::Unsupported,
-			format!("Filesystem \"{}\" is unsupported", fstype.as_ref()),
-		));
-	}
-	Mount::builder().fstype(fstype.as_ref()).mount(from, to)
+	Ok(Mount::builder().fstype(fs.as_ref()).mount(from, to)?)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,149 @@
-use android_chroot::Arguments;
-use clap::Parser;
-use std::fs::{write, File};
-use std::io::Result;
-use std::path::PathBuf;
+// #[cfg(not(any(target_os = "android", debug_assertions)))]
+// compile_error!("Only android is supported");
+
+use android_chroot::{mount_bind, mount_fs, validate_file /* , Arguments */};
+use anyhow::Result;
+// use clap::Parser;
+use flate2::read::GzDecoder;
+use loopdev::{LoopControl, LoopDevice};
+use std::{
+	fs::{create_dir, read_to_string, remove_file, write, File},
+	io::copy,
+	path::Path,
+	process::{Command, Stdio},
+	time::Duration,
+};
+use sys_mount::{unmount, Unmount, UnmountFlags};
+use tar::Archive;
 use unbytify::unbytify;
+use ureq::AgentBuilder;
 use url::Url;
 
-#[cfg(not(any(target_os = "android", debug_assertions)))]
-compile_error!("Only android is supported");
-
 fn install(
-	root_size: &String,
-	url_rootfs: &Option<Url>,
-	path_rootfs: &PathBuf,
-	disk_img_path: PathBuf,
-	fs_type: String,
-	root_path: PathBuf,
+	root_size: impl AsRef<str>, url_tar_rootfs: Option<Url>, path_tar_rootfs: impl AsRef<Path>,
+	root_path: impl AsRef<Path>,
 ) -> Result<()> {
-	let size_bytes = unbytify(&root_size).expect(&format!("Could not parse: \"{}\"", root_size));
-	println!("{size_bytes} bytes");
-	let file = File::create(disk_img_path)?;
-	file.set_len(size_bytes)?;
+	let root_path = validate_file(root_path, true, false)?;
+	let img_path = validate_file(root_path.parent().unwrap().join("disk.img"), false, false)?;
+	let mut path_tar_rootfs = path_tar_rootfs.as_ref().canonicalize()?;
+	create_dir(&root_path)?;
+	path_tar_rootfs.set_extension("tar.gz");
+	if let Some(url_rootfs) = url_tar_rootfs {
+		path_tar_rootfs = root_path.parent().unwrap().join("rootfs.tar.gz");
+		let mut tar = File::create(&path_tar_rootfs)?;
+		copy(
+			&mut AgentBuilder::new()
+				.timeout_read(Duration::from_secs(5))
+				.timeout_write(Duration::from_secs(5))
+				.build()
+				.get(url_rootfs.as_ref())
+				.call()?
+				.into_reader(),
+			&mut tar,
+		)?;
+	}
+	let tar_gz = File::open(path_tar_rootfs)?;
+	let tar = GzDecoder::new(tar_gz);
+	let mut archive = Archive::new(tar);
+
+	let size_bytes = unbytify(root_size.as_ref())?;
+	let disk_img = File::create(&img_path)?;
+	disk_img.set_len(size_bytes)?;
+	Command::new("mke2fs")
+		.args(["-t", "ext4", img_path.to_str().unwrap()])
+		.stderr(Stdio::null())
+		.stdout(Stdio::null())
+		.spawn()?
+		.wait()?;
+	let loop_device = LoopControl::open()?.next_free()?;
+	loop_device.attach_file(&img_path)?;
+	let _mount = mount_fs(&img_path, &root_path, "ext4")?.into_unmount_drop(UnmountFlags::EXPIRE);
+	archive.unpack(&root_path)?;
+	create_dir(root_path.join("sdcard"))?;
 	Ok(())
 }
 
-fn resize(
-	new_size: &String,
-	disk_img_path: PathBuf,
-	fs_type: String,
-	root_path: PathBuf,
+// TODO: this
+// fn resize(new_size: &String, root_path: impl AsRef<Path>) -> Result<()> {
+// 	let root_path = validate_file(root_path, true, true)?;
+// 	let img_path = validate_file(root_path.parent().unwrap().join("disk.img"),
+// false, true)?;
+
+// 	validate_file(root_path.parent().unwrap().join("loopdevice.lock"), false,
+// false)?;
+
+// 	Ok(())
+// }
+
+fn mount(root_path: impl AsRef<Path>) -> Result<()> {
+	let root_path = validate_file(root_path, true, true)?;
+	let img_path = validate_file(root_path.parent().unwrap().join("disk.img"), false, true)?;
+	let loop_device = LoopControl::open()?.next_free()?;
+	loop_device.attach_file(&img_path)?;
+	mount_fs(&img_path, &root_path, "ext4")?;
+	mount_bind("/dev", &root_path.join("dev"))?;
+	mount_fs("proc", &root_path.join("proc"), "proc")?;
+	mount_fs("sysfs", &root_path.join("sys"), "sysfs")?;
+	mount_fs("tmpfs", &root_path.join("tmp"), "tmpfs")?;
+	mount_fs("devpts", &root_path.join("dev/pts"), "devpts")?;
+	mount_bind("/sdcard", &root_path.join("sdcard"))?;
+	write(
+		root_path.parent().unwrap().join("loopdevice.lock"),
+		loop_device.path().unwrap().to_str().unwrap(),
+	)?;
+	Ok(())
+}
+
+fn umount(root_path: impl AsRef<Path>) -> Result<()> {
+	let root_path = validate_file(root_path, true, true)?;
+	unmount(&root_path.join("sdcard"), UnmountFlags::EXPIRE)?;
+	unmount(&root_path.join("dev/pts"), UnmountFlags::EXPIRE)?;
+	unmount(&root_path.join("tmp"), UnmountFlags::EXPIRE)?;
+	unmount(&root_path.join("sys"), UnmountFlags::EXPIRE)?;
+	unmount(&root_path.join("proc"), UnmountFlags::EXPIRE)?;
+	unmount(&root_path.join("dev"), UnmountFlags::EXPIRE)?;
+	unmount(&root_path.join("root"), UnmountFlags::EXPIRE)?;
+	let lock_path = root_path.parent().unwrap().join("loopdevice.lock");
+	let loop_device = LoopDevice::open(read_to_string(&lock_path)?)?;
+	loop_device.detach()?;
+	remove_file(lock_path)?;
+	Ok(())
+}
+
+fn start(
+	root_path: impl AsRef<Path>, user: impl AsRef<str>, shell: impl AsRef<Path>,
 ) -> Result<()> {
-	todo!()
-}
-
-fn mount(disk_img_path: PathBuf, fs_type: String, root_path: PathBuf) -> Result<()> {
-	todo!()
-}
-
-fn umount(disk_img_path: PathBuf, fs_type: String, root_path: PathBuf) -> Result<()> {
-	todo!()
-}
-
-fn start(disk_img_path: PathBuf, fs_type: String, root_path: PathBuf) -> Result<()> {
-	todo!()
+	let root_path = validate_file(root_path, true, true)?;
+	validate_file(root_path.join(shell.as_ref().strip_prefix("/")?), false, true)?;
+	mount(&root_path)?;
+	Command::new("unshare")
+		.env_clear()
+		.env("TERM", "xterm-256color")
+		.args([
+			"--uts",
+			"env",
+			"-i",
+			"chroot",
+			"-u",
+			user.as_ref(),
+			root_path.to_str().unwrap(),
+			shell.as_ref().to_str().unwrap(),
+		])
+		.stderr(Stdio::inherit())
+		.stdin(Stdio::inherit())
+		.stdout(Stdio::inherit())
+		.spawn()?
+		.wait()?;
+	println!("done");
+	umount(root_path)?;
+	Ok(())
 }
 
 fn main() -> Result<()> {
-	let args = Arguments::parse()
-		.autofill(
-			Arguments::load_from_file("./android-chroot.toml")?.autofill(
-				Arguments::load_from_file("$HOME/.config/android-chroot.toml")?,
-			),
-		)
-		.validate()?;
-	let mut args2 = args.clone();
-	args2.command = None;
-	match args.command.as_ref().unwrap() {
-		android_chroot::Subcommands::Install {
-			size_root,
-			url_rootfs,
-			path_rootfs,
-		} => {
-			write("./android-chroot.toml", toml::to_string(&args2).unwrap())?;
-			install(
-				size_root,
-				url_rootfs,
-				path_rootfs,
-				args.disk_img_path.unwrap(),
-				args.fs_type.unwrap(),
-				args.root_path.unwrap(),
-			)
-		}
-		android_chroot::Subcommands::Resize { new_size } => resize(
-			new_size,
-			args.disk_img_path.unwrap(),
-			args.fs_type.unwrap(),
-			args.root_path.unwrap(),
-		),
-		android_chroot::Subcommands::Mount => mount(
-			args.disk_img_path.unwrap(),
-			args.fs_type.unwrap(),
-			args.root_path.unwrap(),
-		),
-		android_chroot::Subcommands::Umount => umount(
-			args.disk_img_path.unwrap(),
-			args.fs_type.unwrap(),
-			args.root_path.unwrap(),
-		),
-		android_chroot::Subcommands::Start => start(
-			args.disk_img_path.unwrap(),
-			args.fs_type.unwrap(),
-			args.root_path.unwrap(),
-		),
-	}
+	install(
+		"10G",
+		Some("https://fl.us.mirror.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz".try_into()?),
+		"",
+		"./root",
+	)
 }
